fdea77530bd243983693459b723cdb791b393474
Bodleum <git@daniellaing.com>
2021-11-24 20:26:56 +0000

    Change prompt to be normal colour scheme

--- dmenu.c
+++ dmenu.c
@@ -211,7 +211,7 @@ drawmenu(void)
     drw_rect(drw, 0, 0, mw, mh, 1, 1);
 
     if (prompt && *prompt) {
-        drw_setscheme(drw, scheme[SchemeSel]);
+        drw_setscheme(drw, scheme[SchemeNorm]);
         x = drw_text(drw, x, 0, promptw, bh, lrpad / 2, prompt, 0);
     }
     /* draw input field */
